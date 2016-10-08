a881bb294391d5ac29b5dab44971615060976146
Flavio Moreira <faio16@gmail.com>
2016-10-08 12:38:43 +0100

    Added UI Bootstrap

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'ui.bootstrap',
   'infinite-scroll',
   'myApp.projects',
   'myApp.people',
